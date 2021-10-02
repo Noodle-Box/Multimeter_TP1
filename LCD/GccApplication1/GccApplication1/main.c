7de167693ac8ed36710262edddbb5b5885515231
mayvin <mayvinr2001@gmail.com>
2021-10-02 17:03:15 +1000

    basic lCD code, similar to what was in the video. Buttons needs to be adjusted to new port

---
+++ LCD/GccApplication1/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 7/09/2021 3:12:22 PM
+ * Author : mayvi
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ LCD/LCD/LCD/main.c
@@ -0,0 +1,100 @@
+/*
+ * LCD.c
+ *
+ * Created: 7/09/2021 2:57:07 PM
+ * Author : mayvin
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include "LCD_LIB/lcd.h"
+#include <stdint.h>
+
+
+	void change_brightness (int level) {
+		
+		if (level <= 5){
+			/* clear the bar */
+			for (int i = 0; i < 5; i++){
+				lcd_gotoxy((11 + i),4);
+				lcd_puts(" ");
+			}
+			
+			/* fill it up to the level desired */
+			for (int i=0; i < level; i++) {
+				lcd_gotoxy((11 + i),4);
+				lcd_putc(255);
+			}
+		}
+	}
+
+
+int main(void)
+{	
+	lcd_init(LCD_DISP_ON_CURSOR);
+	
+	/* set port B as output*/
+	 DDRB &= 0xF9; */
+	
+	/* relevant bitshift to activate pullup resistor*/
+	PORTB = (1 << PORTB1) | (1 << PORTB2); 
+	
+
+
+
+	/*clear the LCD screen*/
+	lcd_clrscr();
+
+	/*create the bar and relevant messages*/
+	lcd_gotoxy(11,4);           
+	lcd_putc(255);       
+	lcd_gotoxy(12,4);         
+	lcd_putc(255); 
+	lcd_gotoxy(13,4);
+	lcd_putc(255);
+	lcd_gotoxy(14,4);
+	lcd_putc(255);
+	lcd_gotoxy(15,4);
+	lcd_putc(255); 
+
+	lcd_gotoxy(0,0);
+	lcd_puts("Hi i am LCD");
+	
+	lcd_gotoxy(0,4);
+	lcd_puts("Brightness:");
+	
+	int current_level = 5;
+	
+    /* Replace with your application code */
+    while (1) 
+    {
+		/* if the pin is low decrease the brightness */
+		if((PINB & (1 << PINB1)) == 0){
+			_delay_ms(250);
+			if (current_level - 1 == 0){
+				current_level = 5;
+			} else {
+				current_level --;
+			}
+			change_brightness(current_level);
+		}
+			
+			
+		/* if the pin is high increase the brightness */
+		if((PINB & (1 << PINB2)) == 0){
+			_delay_ms(250);
+			if(current_level + 1 > 5){
+				current_level = 5;
+			} else {
+				current_level ++;
+			}
+			change_brightness(current_level);
+		}
+			
+		
+	}
+	
+ 
+	
+}
+
