5c7c4ea845ac1c1af5305f80570a2e5441a9eb08
mayvin <mayvinr2001@gmail.com>
2021-10-06 01:09:43 +1000

    Completely new gui with partial pyserial implementation, can select ports but unsure if they actually connect and having problems with readSerial

---
+++ GUIV2I1.py
@@ -0,0 +1,134 @@
+from tkinter import *
+from tkinter import messagebox
+from threading import Thread
+import serial.tools.list_ports;
+import serial
+import tkinter as tk
+
+hold = 0
+currentmode = "DC"
+minimum = 0
+maximum = 0
+current = 0 #current reading
+offset = 1 #dc offset data
+continuity = 2.0 #continuity value
+
+ports = [comport.device for comport in serial.tools.list_ports.comports()]
+port = serial.Serial()
+baudrate = 2400
+ser = NONE
+
+foundport = False
+resetflag = 0
+
+def read_serial():
+    global hold
+    global resetflag
+    global port
+
+    #while True:
+    #    if port:
+    #        if not hold:
+    #            try:
+    #                ser_string = str(port.readline())
+    #                ser_string[ser_string.index('[') + 1:ser_string.index(']')].split(',')
+    #            except serial.serialutil.SerialException:
+    #                pass
+
+class GUI:
+    def __init__(self,master):
+        self.master = master
+
+        #Relevant menus for buttons
+        #backlight level
+        LEVELS = [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"
+        ]
+
+        #Create buttons for modes
+        self.DCButton = Button(master, text="DC", command=lambda: self.dc_voltage())
+        self.ACButton = Button(master, text="AC", command=lambda: self.AC())
+        self.ResButton = Button(master, text="Resistance", command=lambda: self.Resistance())
+        self.ContButton = Button(master, text="Continuity", command=lambda: self.Cont())
+        self.HoldButton = Button(master, text="Hold", command=lambda: self.toggleHold())
+        self.ResetButton = Button(master, text="Reset")
+        PortButton = tk.Button(master, text="Connect",command=self.connectport)
+
+        #OPTIONMENU FOR BRIGHTNESS
+        self.var = StringVar()
+        self.var.set(LEVELS[4])
+        self.lightmenu = tk.OptionMenu(master, self.var, *LEVELS)
+        self.var_port = StringVar()
+        portnamefield = tk.OptionMenu(master, self.var_port,*ports)
+        
+        #LABELS
+        self.modelabel = Label(master,text="Current Mode: DC")
+        self.lightlabel = Label(master,text="Brightness")
+        self.minlabel = Label(master, text="Minimum: " + str(minimum))
+        self.maxlabel = Label(master, text ="Maximum: " + str(maximum))
+        self.data = Label(master, text= "Current Reading: " + str(current))
+        portnamelabel = tk.Label(master, text="Select Port: ")
+
+        #Grid the buttons on
+        self.lightlabel.grid(row=0,column=0)
+        self.lightmenu.grid(row=0,column=1)
+        portnamelabel.grid(row=0,column=3)
+        portnamefield.grid(row=0,column=4)
+        PortButton.grid(row=0,column=5)
+        self.DCButton.grid(row=1,column=1)
+        self.ACButton.grid(row=1,column=2)
+        self.ResButton.grid(row=1,column=3)
+        self.ContButton.grid(row=1,column=4)
+        self.modelabel.grid(row=1,column=0)
+        self.data.grid(row=3,column=0)
+        self.minlabel.grid(row=4,column=0)
+        self.maxlabel.grid(row=5,column=0)
+        self.ResetButton.grid(row=6,column=0)
+        self.HoldButton.grid(row=6,column=1)
+
+    def dc_voltage(self):
+        currentmode = "DC"
+        self.modelabel.config(text= "Current Mode: DC")
+
+    def AC(self):
+        currentmode = "AC"
+        self.modelabel.config(text= "Current Mode: AC")
+        self.data.config(text=   "DC Offset: " + str(offset)   + "   AC Voltage: " + str(current) + " Vrms")
+
+    def Resistance(self):
+        currentmode = "Resistance"
+        self.modelabel.config(text= "Current Mode: Resistance")
+        self.data.config(text="Resistance: " + str(current))
+
+    def Cont(self):
+        currentmode = "Contintuity"
+        self.modelabel.config(text= "Current Mode: Continuity")
+        self.data.config(text="Current reading: " + str(current))
+
+    def toggleHold(self):
+        global hold
+        hold ^= 1
+        print(hold)
+
+    def connectport(self):
+        #initialise port params
+        global port
+        global baudrate
+
+        portname = self.var_port.get()
+
+        try:
+            port= serial.Serial(portname,baudrate)
+            portconlabel = tk.Label(self.master, text="Connected to\n" + portname)
+            portconlabel.grid(row=6,column=3)
+        except serial.serialutil.SerialException:
+            port = None
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    GUI(root)
+    root.mainloop()
